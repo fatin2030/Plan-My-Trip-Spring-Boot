0dae640ce2988444035b03c205faa7ad938de04b
Fatin Noor <fatinnoor2030@gmail.com>
2025-09-28 22:57:43 +0600

    feat : AUTH

--- src/main/java/com/fatin_noor/planmytrip/auth/exception/ApiException.java
+++ src/main/java/com/fatin_noor/planmytrip/auth/exception/ApiException.java
@@ -1,5 +1,8 @@
 package com.fatin_noor.planmytrip.auth.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ApiException extends RuntimeException {
     private final int statusCode;
 
@@ -8,7 +11,4 @@ public class ApiException extends RuntimeException {
         this.statusCode = statusCode;
     }
 
-    public int getStatusCode() {
-        return statusCode;
-    }
 }
